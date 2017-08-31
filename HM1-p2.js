a25ed476981bac04bb7927f4a0bd5195528109f7
abudDubosh <31387098+abudDubosh@users.noreply.github.com>
2017-08-31 16:22:58 -0500

    Add files via upload

---
+++ HM1-p2.js
@@ -0,0 +1,60 @@
+let x= math.round(7.25);
+colors= ['red','green','blue'];
+let n = colors.length;
+
+
+
+
+let x = 9;
+let y = 'hello';
+
+if ( typeof x == typeof y) {
+	console.log('Same type');
+}
+else
+{
+	console.log('Not the same type')}
+
+
+
+let x = 7;
+x = x % 3; 
+
+
+
+let arr = [1, "red", 2, "yellow"];
+
+if(6/0 == 10/0) {
+	console.log('Equals');
+}
+else
+	{console.log('Not Equals');
+}
+
+
+
+
+//
+//The questions:
+//Declare a variable x and initialize it with an integer.
+//
+//How do you round the number 7.25, to the nearest integer?
+//
+//Create a array called colors with the strings red, green and blue inside.
+//
+//How can you find the length of the string you just created?
+//
+//Write a program that checks the types of two variables and prints out SAME TYPE if they are the same type. For example:
+//
+//let x = 9;
+//let y = 'Hello';
+//
+//if () {
+//  console.log('SAME TYPE');
+//}
+//If x equals 7, and the only other statement is x = x % 3, what would be the new value of x?
+//
+//Write a program to answer the following questions:
+//
+//Can you store multiple types in an array? Numbers and strings?
+//Can you compare inifities? (Not in Eyad's world) - does 6/0 == 10/0? How can you test this?
